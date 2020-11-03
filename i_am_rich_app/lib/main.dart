9d81126b79da5ec01ce08e833ae9e6491f3240d1
dinjit <dinjit.dinesh@yocket.in>
2020-11-03 23:35:34 +0530

    MICard

--- i_am_rich_app/lib/main.dart
+++ i_am_rich_app/lib/main.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text('Hello World'),
+    home: Scaffold(
+      backgroundColor: Colors.orange,
+      appBar: AppBar(
+        title: Text('I Am Rich'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
+      ),
     ),
   ));
 }
